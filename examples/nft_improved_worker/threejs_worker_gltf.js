7ad111fbb4f84973d21b5a4f86dd366d58366e89
Дмитрий <timon1703@mail.ru>
2020-12-13 18:54:52 +0700

    Update threejs_worker_gltf.js

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -89,7 +89,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
     /* Load Model */
     var threeGLTFLoader = new THREE.GLTFLoader();
 
-    threeGLTFLoader.load("../Data/models/heart.glb", function (gltf) {
+    threeGLTFLoader.load("../Data/models/Alien.glb", function (gltf) {
             model = gltf.scene.children[0];
             model.position.z = 0;
 			model.scale.z = 100
@@ -98,13 +98,13 @@ function start( container, marker, video, input_width, input_height, canvas_draw
             model.position.y = 0;
 			model.scale.y = 100
 
-           
+           /*
 			var animation = gltf.animations[0];
             var mixer = new THREE.AnimationMixer(model);
             mixers.push(mixer);
             var action = mixer.clipAction(animation);
             action.play();
-			
+			*/
             root.matrixAutoUpdate = false;
             root.add(model);
         }
