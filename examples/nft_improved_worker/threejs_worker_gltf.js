4746b53bc977df6f3afe1eea08eb3a635200a952
Дмитрий <timon1703@mail.ru>
2020-12-13 23:45:48 +0700

    Update threejs_worker_gltf.js

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -6,7 +6,7 @@ function isMobile() {
     return /Android|mobile|iPad|iPhone/i.test(navigator.userAgent);
 }
 
-var interpolationFactor = 50;
+var interpolationFactor = 24;
 
 var trackedMatrix = {
     // for interpolation
